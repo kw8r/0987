83b35de5c922bdc82a25114a14812b7de3010315
kk <ken.kw8@gmail.com>
2014-01-07 20:10:30 +0900

    0107

---
+++ functions.php
@@ -0,0 +1,79 @@
+<?php
+function bigflag_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Main Sidebar', 'bigflag' ),
+		'id' => 'sidebar-1',
+		'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'bigflag' ),
+		'before_widget' => '<aside id="%1$s" class="%2$s">',
+		'after_widget' => "\n</aside>",
+		'before_title' => "\n<h3 class=\"widget-title\">",
+		'after_title' => "</h3>\n",
+	) );
+}
+add_action( 'widgets_init', 'bigflag_widgets_init' );
+
+/**
+ * 20131204追記
+ *
+ */
+/* パスを自動的にテーマディレクトリまでのパスに置換してくれる(img) */
+function replaceImagePath($arg) {
+	$content = str_replace('"pdf/', '"' . get_bloginfo('template_directory') . '/pdf/', $arg);
+	return $content;
+}  
+add_action('the_content', 'replaceImagePath');
+/*ここまで*/
+
+/* パスを自動的にテーマディレクトリまでのパスに置換してくれる(img) */
+function replaceImagePath2($arg) {
+	$content2 = str_replace('"img/', '"' . get_bloginfo('template_directory') . '/img/', $arg);
+	return $content2;
+}  
+add_action('the_content', 'replaceImagePath2');
+/*ここまで*/
+
+// フッターWordPressリンクを非表示に
+function custom_admin_footer() {
+ echo '<a href="mailto:xxx@zzz"></a>';
+ }
+add_filter('admin_footer_text', 'custom_admin_footer');
+
+/* 画像挿入時リンクタグが入らないように */
+function remove_a_tag_image_send_to_editor($html, $id, $caption, $title, $align, $url, $size) {
+  return strip_tags($html, '<img>');
+}
+add_filter('image_send_to_editor', 'remove_a_tag_image_send_to_editor', 10, 7);
+
+/* 画像挿入時高さと幅が入らないように */
+function remove_hwstring_from_image_tag( $html, $id, $alt, $title, $align, $size ) {
+	if ( !isset ( $infomation_width ) ) {
+		$infomation_width = 715;
+	}
+    list( $img_src, $width, $height ) = image_downsize($id, $size);
+	if ( $width > 	$infomation_width ) {
+	    $hwstring = image_hwstring( $width, $height );
+	    $html = str_replace( $hwstring, '', $html );
+	}
+    return $html;
+}
+add_filter( 'get_image_tag', 'remove_hwstring_from_image_tag', 10, 6 );
+
+//プレビューボタン非表示
+add_action('admin_print_styles', 'admin_preview_css_custom');
+function admin_preview_css_custom() {
+ if (!current_user_can('level_10')) { //level10以下のユーザーの場合ウィジェットをunsetする
+   echo '<style>span.view,#preview-action {display: none;}</style>';
+ }
+}
+
+//管理画面系
+require( dirname( __FILE__ ) . '/inc/management.php' );
+//カスタム投稿タイプ追加
+require( dirname( __FILE__ ) . '/inc/custom-posts.php' );
+// メニューを非表示にする
+require( dirname( __FILE__ ) . '/inc/menu-hide.php' );
+//pdfアップローダー追加
+require( dirname( __FILE__ ) . '/inc/pdf-uploader.php' );
+
+
+?>
---
+++ media_query.php
@@ -0,0 +1,40 @@
+﻿//functions.phpにでも書いておく
+add_filter('query_string', "set_media_query");
+
+function set_media_query($query_string){
+	global $pagenow,$current_user; // media-upload.php 、upload.php
+
+	// メディアライブラリ系の処理以外は何もしない
+	if($pagenow != 'media-upload.php' && $pagenow != 'upload.php') {
+		return $query_string;
+	}
+
+	get_currentuserinfo();
+
+	// 編集者以上だったら何もしない
+	if($current_user->user_level >= 5) {
+		return $query_string;
+	}
+
+	wp_parse_str($query_string, $qv);
+
+	// メディア以外のqueryの場合も何もしない
+	if($qv['post_type'] != 'attachment') {
+		return $query_string;
+	}
+
+	// 条件に自分の画像を追加
+	$add_query_string = array(
+		'author' => $current_user->ID
+	);
+
+	$new_query_vars = array_merge($add_query_string,$qv);
+
+	$query_var = array();
+	foreach ($new_query_vars as $key => $value) {
+		$query_var[] = $key.'='.$value;
+	}
+
+	return join('&', $query_var);
+
+}
